640f8f75fa543925c04819510bd51155b541d672
chzchzchz <chzchzchz@users.noreply.github.com>
2021-05-27 00:18:24 -0700

    pals: --paldir flag

--- hack/pals/pals.go
+++ hack/pals/pals.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"flag"
+	"image"
 	"math/rand"
 	"os"
+	"path/filepath"
+	"sort"
 	"time"
 
 	"github.com/chzchzchz/sitbot/ascii"
@@ -19,47 +23,61 @@ var necks = []string{
 	`
 │ │ `}
 
+// Default pal if no paldir.
 var pals = []string{
-	`
-╭──╮
+	`╭──╮
 o_o│
 ╰╮ │
-
 `,
-	`
-╭──╮
+	`╭──╮
 │o_o
-│ ╭╯`,
-}
-
-var pals2 = []string{
-	`
-╭──╮
-O O│
-╰o │
+│ ╭╯
 `,
-	`
-╭──╮
-│O O
-│ o╯`,
-}
-
-var pals3 = []string{
-	`
-╭──╮
-o o│
-│- │
-╰╮ │`,
-	`
-╭──╮
-│o o
-│ -│
-│ ╭╯`,
 }
 
 var floor = "─"
 
+func mustAscii(s string) *ascii.ASCII {
+	a, err := ascii.NewASCII(s)
+	if err != nil {
+		panic(err)
+	}
+	return a
+}
+
+func loadCaps(dir string) (ret []*ascii.ASCII) {
+	if dir == "" {
+		return []*ascii.ASCII{mustAscii(pals[0]), mustAscii(pals[1])}
+	}
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+	names := []string{}
+	for _, f := range files {
+		if !f.IsDir() {
+			names = append(names, f.Name())
+		}
+	}
+	sort.Strings(names)
+	for _, n := range names {
+		b, err := os.ReadFile(filepath.Join(dir, n))
+		if err != nil {
+			panic(err)
+		}
+		ret = append(ret, mustAscii(string(b)))
+	}
+	return ret
+}
+
 func main() {
+	paldir := ""
+	fill := ""
+	flag.StringVar(&paldir, "paldir", "", "directory of pal caps")
+	flag.StringVar(&fill, "fill", "", "fill string")
+	flag.Parse()
+	caps := loadCaps(paldir)
+
 	rand.Seed(time.Now().UnixNano())
 	a, _ := ascii.NewASCII("")
 	x, y := 70, 14
@@ -69,35 +87,34 @@ func main() {
 			a.PutString(floor, i, y)
 			continue
 		}
-		//		neckHeight := rand.Intn(y-2) - 1
+		n := rand.Intn(2)
+		if rand.Intn(12) == 0 {
+			n = rand.Intn(len(caps))
+		}
+		palcap := caps[n]
+		ph := palcap.Rows()
+
 		stddev := float64(y-2) / 3.0
 		mu := float64(y / 4.0)
 		neckHeight := int(rand.NormFloat64()*stddev + mu)
-		if neckHeight >= y-3 {
-			neckHeight = y - 4
+		if neckHeight >= y-(ph-1) {
+			neckHeight = y - ph
 		} else if neckHeight < 0 {
 			neckHeight = 0
 		}
-		p := rand.Intn(2)
-		if rand.Intn(12) != 0 {
-			a.PutString(pals[p][1:], i, y-4-neckHeight)
-		} else if neckHeight <= y-5 && rand.Intn(12) <= 6 {
-			a.PutString(pals3[p][1:], i, y-5-neckHeight)
-		} else {
-			a.PutString(pals2[p][1:], i, y-4-neckHeight)
-		}
+		a.Paste(palcap, image.Point{i, y - ph - neckHeight})
 		for j := 0; j <= neckHeight; j++ {
-			a.PutString(necks[p][1:], i, (y - neckHeight + j - 1))
+			a.PutString(necks[n%2][1:], i, (y - neckHeight + j))
 		}
-		a.PutString(shoulder[p], i, y)
+		a.PutString(shoulder[n%2], i, y)
 		i += 3
 	}
 
-	if len(os.Args) > 1 {
+	if fill != "" {
 		sidx := 0
 		for j := 0; j < y; j++ {
-			for i := 0; i < x; i++ {
-				v := os.Args[1][sidx%len(os.Args[1])]
+			for i := 0; i < a.Columns(); i++ {
+				v := fill[sidx%len(fill)]
 				if cc := a.Get(i, j); cc != nil {
 					if cc.Value == ' ' {
 						cc.Value = rune(v)
